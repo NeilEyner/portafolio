652f03bae3f17222230d34123db95f12d554e2a3
NeilEyner <neileynerc@gmail.com>
2026-02-28 15:35:45 -0400

    vercel storage

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '3mb',
+    },
+  },
 
   images: {
     remotePatterns: [
-      // Vercel Blob (imágenes subidas en producción)
       {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
       },
-      // Devicons (íconos de tecnologías)
       {
         protocol: 'https',
         hostname: 'cdn.jsdelivr.net',
       },
-      // Simple Icons CDN
       {
         protocol: 'https',
         hostname: 'cdn.simpleicons.org',
       },
     ],
-    // Para imágenes locales NO necesitas configuración extra 
     unoptimized: false,
   },
 };
